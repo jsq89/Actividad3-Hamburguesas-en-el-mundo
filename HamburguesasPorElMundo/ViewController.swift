3e3c67c4ed3c6d84a66cdf444699483daa9b0cbd
Jesus <jesussanchez@jesuss-mac-pro.local>
2016-07-03 17:03:14 +0200

    Initial Commit

---
+++ HamburguesasPorElMundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HamburguesasPorElMundo
+//
+//  Created by Jesus Sanchez on 03/07/16.
+//  Copyright © 2016 Jesus Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
